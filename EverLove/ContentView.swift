dbdabfaa990f5315bdc1b22da8c750547f7a2b8e
Yavuz Selim Güner <yavuzselimguner34@gmail.com>
2024-12-21 15:00:30 +0300

    Initial Commit

---
+++ EverLove/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EverLove
+//
+//  Created by Yavuz Selim Güner on 21.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EverLove/EverLoveApp.swift
@@ -0,0 +1,17 @@
+//
+//  EverLoveApp.swift
+//  EverLove
+//
+//  Created by Yavuz Selim Güner on 21.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct EverLoveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
